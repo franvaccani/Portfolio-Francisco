f11ddeee1a8ddb876517b9d1f1f593eefc05c026
Francisco Vaccani <133238284+franvaccani@users.noreply.github.com>
2024-04-16 22:44:49 -0300

    Add files via upload

--- Javascript/java.js
+++ Javascript/java.js
@@ -4,19 +4,24 @@ btnSwitch.addEventListener('click', () => {
     document.body.classList.toggle('dark');
     btnSwitch.classList.toggle('active');
 });
-
-const sr = ScrollReveal({
-    distance:'65px',
-    duration:2200,
-    delay: 400,
-    reset: true
-});
-
-
-sr.reveal('#profile', {delay:200, origin:'top'});
 function toggleMenu() {
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+const sr = ScrollReveal({
+    distance:'65px',
+    duration:2000,
+    delay: 300,
+    reset: true
+});
+ScrollReveal().reveal('.section__pic-container',{ delay:600});
+ScrollReveal().reveal('.section__text',{ delay:750});
+
+ScrollReveal().reveal('.title',{ delay:300});
+
+ScrollReveal().reveal('.section-conteinerGeneral',{ delay:800});
+ScrollReveal().reveal('.experience-details-container',{ delay:800});
+ScrollReveal().reveal('.contact-info-upper-container',{ delay:800});
+
